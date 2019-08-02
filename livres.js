069aed8cd2019c4e5ef5cb9d460b2b604ec1e957
Yannick Francois <yaf@elsif.fr>
2019-08-02 21:23:57 +0200

    mise à jour

--- livres.js
+++ livres.js
@@ -2,5 +2,9 @@
   {
     auteur: ["Joanna MACY", "Molly YOUNG BROWN "],
     titre: "Ecopsychologie pratique et rituels pour la Terre"
+  },
+  {
+    auteur: ["Gaston Pineau", "Dominique Bachelart", "Dominique Cottereau", "Anne Moneyron"],
+    titre: "Habiter la Terre : Ecoformation terrestre pour une conscience planétaire"
   }
 ]
