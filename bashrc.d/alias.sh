4a980bdf06ae3bbb1501ceb4a82a2c48b073b91a
Yannick Francois <yaf@elsif.fr>
2020-11-07 22:58:05 +0100

    Suppression du fichier inutile d'alias

--- bashrc.d/alias.sh
+++
@@ -1,4 +0,0 @@
-alias elm='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID -p 8000:8000 codesimple/elm:0.19'
-alias exercism='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/code" -u $UID:$GID yaf/exercism:latest'
-alias lein='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID clojure lein'
-
