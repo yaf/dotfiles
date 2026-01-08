e575dbf43d56a527092b66e6eb3ecd3874ec00d1
Yannick François <yannick@kaz.bzh>
2026-01-08 21:52:11 +0100

    Update fish config

--- fish/functions/journal.fish
+++ fish/functions/journal.fish
@@ -1,12 +1,12 @@
 function journal
-  set destinationDirectory $site/content/(date +%G)/(date +%m)
+  set destinationDirectory $site/content/(date +%G)/(date +%V)
   if not test -d $destinationDirectory
     mkdir -p $destinationDirectory
   end
   if test -f /var/lock/journal.lock
     echo "journal déjà ouvert"
   else
-    flock -n /var/lock/journal.lock vim $destinationDirectory/index.md
+    flock -n /var/lock/journal.lock hx $destinationDirectory/index.md
     rm -f /var/lock/journal.lock
   end
 end
